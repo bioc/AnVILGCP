5056fade1ea8c1029a99207fa5f4f32a46a43541
LiNk-NY <marcel.ramos@roswellpark.org>
2024-02-05 18:27:06 -0500

    use utilities from AnVILBase

--- R/avworkspace.R
+++ R/avworkspace.R
@@ -27,24 +27,6 @@ NULL
     }
 })
 
-.avworkspaces_clean <- function(.data) {
-    .data |>
-        select(
-            name = .data$workspace.name,
-            lastModified = .data$workspace.lastModified,
-            createdBy = .data$workspace.createdBy,
-            namespace = .data$workspace.namespace,
-            accessLevel = .data$accessLevel
-        ) |>
-        mutate(
-            lastModified = as.Date(.data$lastModified)
-        ) |>
-        arrange(
-            .data$name,
-            desc(.data$lastModified)
-        )
-}
-
 #' @rdname avworkspace
 #'
 #' @description `avworkspaces()` returns a tibble with available
@@ -62,8 +44,8 @@ avworkspaces <-
     response <- Terra()$listWorkspaces()
     .avstop_for_status(response, "avworkspaces")
 
-    flatten(response) %>%
-        .avworkspaces_clean()
+    AnVILBase::flatten(response) %>%
+        AnVILBase::avworkspaces_clean()
 }
 
 #' @rdname avworkspace
